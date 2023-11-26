84458b88d6223468b479b7d76dc8984176bf2e7d
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-11-26 14:23:11 +0530

    chore: init project
    added redux
    added dummy data

---
+++ src/app/GlobalRedux/Provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { store } from "@/app/GlobalRedux/store";
+import { FC } from "react";
+import { Provider } from "react-redux";
+
+type ProvidersProps = {
+    children: React.ReactNode;
+};
+
+export const Providers: FC<ProvidersProps> = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import DATA from "@/app/dummyData.json";
+
+export default function Home() {
+    console.log({ DATA });
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            text
+        </main>
+    );
+}
