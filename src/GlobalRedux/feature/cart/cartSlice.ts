3f14ba730e15492a0e9dbf928edce2f165ee8709
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-11-28 00:55:58 +0530

    feat: updated cart slice to have individual products quantity

--- src/GlobalRedux/feature/cart/cartSlice.ts
+++ src/GlobalRedux/feature/cart/cartSlice.ts
@@ -1,17 +1,18 @@
 "use client";
 
-import { store } from "@/GlobalRedux/store";
 import { Product } from "@/types/product";
 import { createSlice } from "@reduxjs/toolkit";
 
 type CartState = {
     items: Product[];
+    itemsQuantity: Record<string, number>;
     totalQuantity: number;
     totalPrice: number;
 };
 
 const initialState: CartState = {
     items: [],
+    itemsQuantity: {},
     totalQuantity: 0,
     totalPrice: 0,
 };
@@ -23,15 +24,30 @@ export const cartSlice = createSlice({
         addToCart: (state, { payload }) => {
             const product = payload as Product;
 
-            const { items } = state;
+            if (state.itemsQuantity[`${product.id}`]) {
+                state.totalPrice += product.price;
+                state.totalQuantity += 1;
+                state.itemsQuantity[`${product.id}`] += 1;
+                return;
+            }
 
+            const { items } = state;
             items.push(product);
             state.totalQuantity += 1;
+            state.itemsQuantity[`${product.id}`] = 1;
             state.totalPrice += product.price;
         },
 
         removeFromCart: (state, { payload }) => {
             const product = payload as Product;
+
+            let currentProductQuantity = state.itemsQuantity[`${product.id}`];
+
+            if (currentProductQuantity > 1) {
+                currentProductQuantity -= 1;
+
+                return;
+            }
 
             const filteredItems = state.items.filter(
                 (item) => item.id !== product.id
